e9d84d95369c620e252d718002d2742386eecdf5
David Morgan <dmorgan81@gmail.com>
2017-08-28 15:16:55 -0400

    Battery layer

---
+++ src/c/battery-layer.c
@@ -0,0 +1,48 @@
+#include <pebble.h>
+#include <pebble-events/pebble-events.h>
+#include "logging.h"
+#include "battery-layer.h"
+
+typedef struct {
+    BatteryChargeState charge_state;
+    EventHandle battery_state_event_handle;
+} Data;
+
+static void prv_update_proc(BatteryLayer *this, GContext *ctx) {
+    logf();
+    Data *data = layer_get_data(this);
+    GRect bounds = layer_get_bounds(this);
+
+    uint8_t w = bounds.size.w * data->charge_state.charge_percent / 100;
+    uint8_t x = (bounds.size.w - w) / 2;
+
+    graphics_context_set_stroke_color(ctx, PBL_IF_COLOR_ELSE(GColorYellow, GColorWhite));
+    graphics_context_set_stroke_width(ctx, 2);
+    graphics_draw_line(ctx, GPoint(x, 0), GPoint(x + w, 0));
+}
+
+static void prv_battery_state_handler(BatteryChargeState charge_state, void *this) {
+    logf();
+    Data *data = layer_get_data(this);
+    memcpy(&data->charge_state, &charge_state, sizeof(BatteryChargeState));
+    layer_mark_dirty(this);
+}
+
+BatteryLayer *battery_layer_create(GRect frame) {
+    logf();
+    BatteryLayer *this = layer_create_with_data(frame, sizeof(Data));
+    layer_set_update_proc(this, prv_update_proc);
+    Data *data = layer_get_data(this);
+
+    prv_battery_state_handler(battery_state_service_peek(), this);
+    data->battery_state_event_handle = events_battery_state_service_subscribe_context(prv_battery_state_handler, this);
+
+    return this;
+}
+
+void battery_layer_destroy(BatteryLayer *this) {
+    logf();
+    Data *data = layer_get_data(this);
+    events_battery_state_service_unsubscribe(data->battery_state_event_handle);
+    layer_destroy(this);
+}
--- src/c/main.c
+++ src/c/main.c
@@ -2,11 +2,13 @@
 #include "logging.h"
 #include "hour-layer.h"
 #include "minute-layer.h"
+#include "battery-layer.h"
 #include "date-layer.h"
 
 static Window *s_window;
 static HourLayer *s_hour_layer;
 static MinuteLayer *s_minute_layer;
+static BatteryLayer *s_battery_layer;
 static DateLayer *s_date_layer;
 
 static void prv_window_load(Window *window) {
@@ -19,6 +21,9 @@ static void prv_window_load(Window *window) {
     s_minute_layer = minute_layer_create(GPoint(126, 10));
     layer_add_child(root_layer, s_minute_layer);
 
+    s_battery_layer = battery_layer_create(GRect(12, 100, PBL_DISPLAY_WIDTH - 24, 2));
+    layer_add_child(root_layer, s_battery_layer);
+
     s_date_layer = date_layer_create(GPoint(0, 120));
     layer_add_child(root_layer, s_date_layer);
 
@@ -28,6 +33,7 @@ static void prv_window_load(Window *window) {
 static void prv_window_unload(Window *window) {
     logf();
     date_layer_destroy(s_date_layer);
+    battery_layer_destroy(s_battery_layer);
     minute_layer_destroy(s_minute_layer);
     hour_layer_destroy(s_hour_layer);
 }
